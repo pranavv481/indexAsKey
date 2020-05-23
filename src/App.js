b5e004439e9a7948abc0704713d51f943f2c5a39
pranavv481 <pranavv481@gmail.com>
2020-05-23 17:43:05 +0530

    index as key

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import NameList from './components/NameList';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NameList />
+
+      </div>
+    );
+  }
+}
+
+export default App;
